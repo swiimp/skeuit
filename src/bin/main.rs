43c05ee8f80b24d5edeefe7ac02b9ab320883f88
Robin Markowitz <robin.mwitz@gmail.com>
2022-03-24 12:52:01 -0700

    Move Packet to its own file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,7 +8,6 @@ use std::{
     // net::SocketAddr,
     sync::Mutex,
 };
-
 // use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool, /* PooledConnection */};
@@ -17,8 +16,7 @@ use futures_util::{SinkExt, StreamExt};
 use serde_json::Value;
 use tokio::net::TcpStream;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream};
-
-use skeuit::establish_connection;
+use skeuit::{establish_connection, packet::Packet};
 // use skeuit::models::*;
 
 struct Bot {
@@ -28,13 +26,7 @@ struct Bot {
     session_id: String,
     database: Pool<ConnectionManager<PgConnection>>,
     stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
-}
-
-struct Packet {
-    op: u64,
-    d:  String,
-    t:  String,
-    s:  u64,
+    connected: bool,
 }
 
 impl Bot {
@@ -48,11 +40,23 @@ impl Bot {
             session_id: "".to_owned(),
             database: pool,
             stream: ws,
+            connected: false,
         }
     }
 
     pub fn run(&self) {
+        // create mpsc
+        // create interval
 
+        // create read thread
+
+        // write loop
+            // if connected
+                // heartbeat on interval
+                // on task, spawn thread
+            // else
+                // break;
+        // reconnect
     }
 
     async fn get_sequence_number(&self) -> u64 {
@@ -62,38 +66,9 @@ impl Bot {
     }
 }
 
-impl Packet {
-    pub fn new(op_code: u64, data: String, transaction_type: String, seq_num: u64) -> Packet {
-        Packet{ op: op_code, d: data, t: transaction_type, s: seq_num}
-    }
-
-    pub fn from(packet_data: String) -> Packet {
-        let json: Value = serde_json::from_str(&packet_data).unwrap();
-        let mut seq_num: u64 = 0;
-        if serde_json::to_string(&json["s"]).unwrap() != "null" {
-            seq_num = serde_json::to_string(&json["s"]).unwrap().parse::<u64>().unwrap();
-        }
-        Packet{
-            op: serde_json::to_string(&json["op"]).unwrap().parse::<u64>().unwrap(),
-            d: serde_json::to_string(&json["d"]).unwrap(),
-            t: serde_json::to_string(&json["t"]).unwrap(),
-            s: seq_num,
-        }
-    }
-
-    pub fn to_string(&self) -> String {
-        let result: String;
-        if self.op != 0 {
-            result = format!("\"op\":{},\"d\":{},\"t\":null,\"s\":null", self.op, self.d);
-        } else {
-            result = format!("\"op\":{},\"d\":{},\"t\":{},\"s\":{}", self.op, self.d, self.t, self.s);
-        }
-        "{".to_owned() + &result + "}"
-    }
-}
-
 fn extract_heartbeat(init_packet: Packet) -> u64 {
-
+    let data: Value = serde_json::from_str(&(init_packet.clone()).d).unwrap();
+    serde_json::to_string(&data["heartbeat_interval"]).unwrap().parse::<u64>().unwrap()
 }
 
 #[tokio::main]
@@ -122,10 +97,12 @@ async fn main() {
     let addr = url::Url::parse(&(resp["url"].clone() + "?v=9&encoding=json")).expect("Received bad url");
     let (mut ws_stream, _) = connect_async(&addr).await.expect("Failed to connect");
     let init_request = Packet::new(1, "null".to_owned(), "null".to_owned(), 0);
-    let init_msg = ws_stream.next().await.expect("Failed to parse a response");
+    let init_msg = ws_stream.next().await.unwrap().expect("Failed to parse a response");
     let resp_packet = Packet::from(init_msg.into_text().unwrap());
+    println!("{}", resp_packet.to_string());
     let heartbeat_interval = extract_heartbeat(resp_packet);
+    println!("heartbeat_interval = {}", heartbeat_interval);
 
     let bot = Bot::new(token, seq_num, heartbeat_interval, pool, ws_stream);
-    bot.run();
+    // bot.run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod models;
 pub mod schema;
+pub mod packet;
 
 #[macro_use]
 extern crate diesel;
